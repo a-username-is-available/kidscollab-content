cc18835e5e613202a0c763cd9d6f444a167f5fdb
a-username-is-available <110326358+a-username-is-available@users.noreply.github.com>
2023-11-09 10:34:49 +0000

    Add uuid for each article

--- index.mjs
+++ index.mjs
@@ -1,12 +1,13 @@
 // im using node instead of deno bc im not bothered to change container
 
 import { marked } from "marked";
+import { v5 } from "uuid";
 import { lstat, mkdir, readFile, readdir, writeFile } from "node:fs/promises";
-// import path, { basename, relative } from "node:path";
-// import { cwd } from "node:process";
+import { normalize } from "node:path";
 
 const IN = './content/';
 const OUT = './dist/';
+const UUID_NAMESPACE = '866cb8d6-ce7f-463d-ae10-666464fd2326';
 
 const structure = [];
 async function transferDirectory(path = '') {
@@ -21,11 +22,12 @@ async function transferDirectory(path = '') {
         }
 
         const baseName = file.split('.')[0];
-        traverseByPath(structure, path).push(baseName);
+        const uuid = v5(normalize(path + '/' + file), UUID_NAMESPACE);
+        traverseByPath(structure, path).push({ name: baseName, uuid });
     
         const html = marked(await readFile(newPath, { encoding: 'utf-8' }));
         await mkdir(OUT + '/' + path , { recursive: true });
-        await writeFile(OUT + '/' + path + '/' + fixName(baseName) + '.html', html); // Yes i know `${thing}other` exists im lazy
+        await writeFile(OUT + '/' + path + '/' + fixName(baseName) + '.html', html); // Yes i know `${thing}other` exists but it creates more noise
     }
 }
 
